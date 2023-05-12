d64d8710d6e7da5ee286e1b3b920f17b9e8d2a90
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-12 16:58:52 +0100

    Updated Block.vue

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block" v-if="showBlock">
+  <div class="block" v-if="showBlock" @click="stopTimer">
     click me
   </div>
 </template>
@@ -23,7 +23,7 @@ mounted() {
 methods: {
     startTimer() {
         this.timer = setInterval( () => {
-            this.reactionTime +10
+            this.reactionTime += 10
         }, 10)
     },
     stopTimer() {
