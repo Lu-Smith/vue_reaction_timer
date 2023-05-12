831fbd2d7f0d45a984669694dce544d7ce91e963
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-12 18:14:09 +0100

    Updated Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,7 @@
     >
     and is open-source on
     <a
-      href="https://github.com/Lu-Smith/vue_weather_app"
+      href="https://github.com/Lu-Smith/vue_reaction_timer"
       target="_blank"
       class="cool-link"
       >GitHub</a
@@ -21,5 +21,41 @@ export default {
 </script>
 
 <style>
+p {
+  padding: 20px 0;
+  font-weight: 500;
+  border-radius: 0 0 40px 40px;
+  background-color: rgb(52, 15, 58);
+  color: #c8efde;
+}
 
+.cool-link {
+  position: relative;
+  text-decoration: none;
+  color: #fbf6f6;
+  transition: all 0.3s ease;
+  letter-spacing: 1px;
+}
+
+.cool-link:before {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 3px;
+  bottom: -5px;
+  left: 0;
+  background-color: #c8efde;
+  visibility: hidden;
+  transform: scaleX(0);
+  transition: all 0.3s ease-in-out 0s;
+}
+
+.cool-link:hover:before {
+  visibility: visible;
+  transform: scaleX(1);
+}
+
+.cool-link:hover {
+  color: #c8efde;
+}
 </style>
