e0f7cf51b77918a4bfc30af4619f369e0c1af869
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-12 18:10:45 +0100

    Updated Block.vue

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -37,12 +37,49 @@ methods: {
 
 <style>
 .block {
-    width: 400px;
-    border-radius: 20px;
-    background: #0faf87;
-    color: white;
-    text-align: center;
-    padding: 100px 0;
-    margin: 40px auto;
+width: 400px;
+height: 400px;
+background: linear-gradient(to bottom, #000000, #0faf87);
+border-radius: 50%;
+box-shadow: 0 0 50px 10px #0faf87;
+text-align: center;
+font-size: 36px;
+color: white;
+display: flex;
+align-items: center;
+justify-content: center;
+margin: 40px auto;
+position: relative;
+overflow: hidden;
+transform-style: preserve-3d;
+transition: all 0.5s ease-in-out;
+}
+
+.block:hover {
+transform: scale(1.2) rotateY(360deg);
+}
+
+.block:before {
+content: '';
+position: absolute;
+top: -50%;
+left: -50%;
+width: 200%;
+height: 200%;
+background: rgba(255, 255, 255, 0.1);
+transform: rotate(45deg);
+z-index: -1;
+}
+
+.block:after {
+content: '';
+position: absolute;
+bottom: -50%;
+right: -50%;
+width: 200%;
+height: 200%;
+background: rgba(255, 255, 255, 0.1);
+transform: rotate(45deg);
+z-index: -1;
 }
 </style>
