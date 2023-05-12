f9a0ba184632daf318d5152ab3a0672a0356e54d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-12 18:13:07 +0100

    Created Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <p>
+    This project was coded by
+    <a href="https://www.lunasmithart.com/" target="_blank" class="cool-link"
+      >Luna Smith</a
+    >
+    and is open-source on
+    <a
+      href="https://github.com/Lu-Smith/vue_weather_app"
+      target="_blank"
+      class="cool-link"
+      >GitHub</a
+    >.
+  </p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
