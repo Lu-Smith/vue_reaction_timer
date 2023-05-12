740c265578177a44104d15eb44d864460335aca1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-12 17:22:29 +0100

    Updated Results.vue

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,10 +1,25 @@
 <template>
     <h2>Reaction time: {{score}}ms</h2>
+    <p>{{rank}}</p>
 </template>
 
 <script>
 export default {
-props: ['score']
+props: ['score'],
+data() {
+    return {
+        rank: null
+    }
+},
+mounted() {
+    if (this.score < 350) {
+        this.rank = 'Ninja Fingers'
+    } else if (this.score < 600) {
+        this.rank = 'Rapid Reflexes'
+    } else {
+        this.rank = 'Snail pace...'
+    }
+} 
 }
 </script>
 
